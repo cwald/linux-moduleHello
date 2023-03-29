6879192df9289fe21366d4f4b6e425c88cca327b
Curtis Wald <curtis.wald@nxp.com>
2023-03-29 14:01:37 -0500

    Example hello module
    Signed-off-by: Curtis Wald <curtis.wald@nxp.com>

---
+++ hello.c
@@ -0,0 +1,26 @@
+/*
+ * hello.c : simple kernel module
+ */
+
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+
+static int __init hello_init (void)
+{
+        printk (KERN_INFO "HELLO WORLD!\n");
+
+        return (0);
+}
+
+static void __exit hello_exit (void)
+{
+        printk (KERN_INFO "Goodbye from HELLO WORLD\n");
+}
+
+module_init(hello_init);
+module_exit(hello_exit);
+
+MODULE_AUTHOR("Curtis Wald <curtis.wald@nxp.com>");
+MODULE_DESCRIPTION("Hello World Module Example");
+MODULE_LICENSE("GPL");
